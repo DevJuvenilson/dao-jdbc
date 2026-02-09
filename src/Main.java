4a58d4f99a432db3505588e91286fa532dbaf43d
Juvenilson Daniel <daniel.ribeiroemails@gmail.com>
2026-02-09 14:09:13 -0300

    Added seller entity

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import model.entities.Department;
+import model.entities.Seller;
 
 void main() {
 
     Department department = new Department(1, "Books");
+    Seller seller = new Seller(2, "Juvenilson", "juve@gmail.com", LocalDate.now(), 2000.0, new Department(2, "TI"));
 
     IO.println(department);
+    IO.println(seller);
 
 }
