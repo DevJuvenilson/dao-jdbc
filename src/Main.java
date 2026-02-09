76405d87d9516fa76549d62c1f9f69107bb26465
Juvenilson Daniel <daniel.ribeiroemails@gmail.com>
2026-02-09 14:20:05 -0300

    Added SellerDaoJDBC and DaoFactory

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -5,6 +7,8 @@ void main() {
 
     Department department = new Department(1, "Books");
     Seller seller = new Seller(2, "Juvenilson", "juve@gmail.com", LocalDate.now(), 2000.0, new Department(2, "TI"));
+
+    SellerDao sellerDao = DaoFactory.createSellerDao();
 
     IO.println(department);
     IO.println(seller);
